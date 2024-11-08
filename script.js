f616129774065b9af705b5849f810ee49f130a2f
AdrianRydin <adrian.rydin@gmail.com>
2024-11-08 09:53:00 +0100

    startade mer på menyn

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 var menuContainer = document.getElementById('menuContainer');
 
 menuContainer.addEventListener('click', function(event) {
-    var target = event.target;
 
-    if (target.tagName === 'A') {
-        alert('You clicked on a link');
-    }
-}
+});
