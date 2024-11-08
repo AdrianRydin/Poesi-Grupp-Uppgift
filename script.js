35d90b407aea0bfcd5d4267ff1a36baeb6fdca87
AdrianRydin <adrian.rydin@gmail.com>
2024-11-08 11:42:03 +0100

    fixade menu

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var menuContainer = document.getElementById('menuContainer');
+window.onload = function() {
 
-menuContainer.addEventListener('click', function(event) {
 
-});
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+    
+    menu_btn.addEventListener('click', function() {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+
+}
